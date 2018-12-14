b217720c4749dfc0684fd79fbfddc97fcc672a75
Trokhymenko <trokhymenko@trokhymenko.local>
2018-12-14 15:00:55 +0200

    update preprocessing

--- nlp/preprocessing/preprocessing_tools.py
+++ nlp/preprocessing/preprocessing_tools.py
@@ -43,10 +43,10 @@ def lemmatization(text):
         [0] если зашел тип не `str` делаем его `str`
         [1] токенизация предложения через razdel
         [2] проверка есть ли в начале слова '-'
-        [3] проверка на стоп-слова
-        [4] проверка токена с одного символа
-        [5] проверка есть ли данное слово в кэше
-        [6] лемматизация слова
+        [3] проверка токена с одного символа
+        [4] проверка есть ли данное слово в кэше
+        [5] лемматизация слова
+        [6] проверка на стоп-слова
         
         на выходе лист отлемматизированых токенов
     '''
@@ -54,7 +54,7 @@ def lemmatization(text):
     # [0]
     if not isinstance(text, str):
         text = str(text)
-
+    
     # [1]
     tokens = list(tokenize(text))
     words = [_.text for _ in tokens]
@@ -63,11 +63,13 @@ def lemmatization(text):
     for w in words:
         if w[0] == '-': # [2]
             w = w[1:]
-        if not w in stopword_ru: # [3]
-            if len(w)>1: # [4]
-                if w in cache: # [5]
-                    words_lem.append(cache[w])
-                else: # [6]
-                    temp_cach = cache[w] = morph.parse(w)[0].normal_form
-                    words_lem.append(temp_cach)
-    return words_lem
+        if len(w)>1: # [3]
+            if w in cache: # [4]
+                words_lem.append(cache[w])
+            else: # [5]
+                temp_cach = cache[w] = morph.parse(w)[0].normal_form
+                words_lem.append(temp_cach)
+
+    words_lem_without_stopwords = [i for i in words_lem if not i in stopword_ru] # [6]
+
+    return words_lem_without_stopwords

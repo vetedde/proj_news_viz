fd0547506e424151c32985004cfc20d267a12e77
vtrokhymenko <trokhymenkoviktor@gmail.com>
2019-02-04 13:38:28 +0200

    add bigartm

---
+++ nlp/topic_models/separateTopics/cleanedNames.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+import pickle
+from flashtext import KeywordProcessor # pip install flashtext
+
+class KeyProc:
+    
+    with open('./data/cleaned_name.pickle', 'rb') as handle:
+        dict_cleaned_name = pickle.load(handle)
+
+    def __init__(self, dict = dict_cleaned_name):
+        #self.text = text
+        self.kp = KeywordProcessor()
+        self.kp.add_keywords_from_dict(dict)
+
+    def extractKeywords(self, text):
+        return self.kp.extract_keywords(text)
+
+    def replaceKeywords(self, text):
+        return self.kp.replace_keywords(text)

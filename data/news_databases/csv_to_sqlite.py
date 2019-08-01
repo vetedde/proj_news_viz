816f1fffd3777fc2fbc914d3935eb304b4723b90
monuvio <kronizzyd@gmail.com>
2019-08-01 10:33:38 +0500

    add name of collected database

--- data/news_databases/csv_to_sqlite.py
+++ data/news_databases/csv_to_sqlite.py
@@ -1,26 +1,30 @@
-import sqlite3
+import re 
 import csv
+import sqlite3
 import argparse
 
 def csv_to_sqlite(filename, dst):
-	conn = sqlite3.connect('{}gazeta.db'.format(dst))
-	cur = conn.cursor() 
-	cur.execute("""CREATE TABLE IF NOT EXISTS gazeta(date varchar,	url varchar,	edition varchar,
-	topics varchar,	authors varchar,	title varchar,	text varchar,	reposts_fb varchar,
-	reposts_vk varchar,	reposts_ok varchar,	reposts_twi varchar,	reposts_lj varchar,	reposts_tg varchar,	
-	likes varchar,	views varchar,	comm_count varchar, whatever varchar)""")
-	filename.encode('utf-8')
-	with open(filename) as f:
-		reader = csv.reader(f)
-		for field in reader:
-		  #some lines were damaged, that's why there are used try/except
-			try:
-				cur.execute("INSERT INTO gazeta VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", field)
-			except:
-				continue
+    try:
+        newsjournal_name = filename[filename.rfind('/')+1:filename.rfind('.')]
+    except:
+        newsjournal_name = 'database'
+    conn = sqlite3.connect(f"{dst}{newsjournal_name}.db")
+    cur = conn.cursor() 
+    cur.execute("""CREATE TABLE IF NOT EXISTS {}(date varchar, url varchar, edition varchar,
+    topics varchar,	authors varchar, title varchar, text varchar, reposts_fb varchar,
+    reposts_vk varchar,	reposts_ok varchar,	reposts_twi varchar, reposts_lj varchar, reposts_tg varchar,	
+    likes varchar, views varchar, comm_count varchar, whatever varchar)""".format(newsjournal_name))
+    with open(filename, encoding='utf-8', newline='') as f:
+        reader = csv.reader(f)
+        for field in reader:
+		    #some lines were damaged, that's why there are used try/except
+            try:
+                cur.execute("INSERT INTO {} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);".format(newsjournal_name), field)
+            except:
+                continue
 
-	conn.commit()
-	conn.close()
+    conn.commit()
+    conn.close()
 		
 if __name__ == '__main__':
 	

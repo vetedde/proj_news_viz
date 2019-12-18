bce996fffceb78b9164aa84de7b792e67ab1cb30
Egor Silaev <silaevegor@hotmail.com>
2019-12-18 20:29:34 +0300

    luigi preprocessing
    Proccess of batch file is complite. Add
    two tasks for luigi. Luigi lib was implimented
    into the process. Add docs for database.

--- nlp/experiments/exp-002_preprocessing_luigi_transfer_to_db/scripts/preprocess.py
+++ nlp/experiments/exp-002_preprocessing_luigi_transfer_to_db/scripts/preprocess.py
@@ -1,5 +1,10 @@
 import os
 import time
+from datetime import datetime
+
+import luigi
+from luigi.contrib.postgres import PostgresTarget
+from luigi.format import UTF8
 
 from razdel import tokenize
 from razdel import sentenize
@@ -11,25 +16,103 @@ import configparser
 from dbconnector import UseDatabase
 
 
-class Preprocessor:
+class WriteDataToDatabase(luigi.Task):
     """
-    Используя luigi читает данные из таблицы сырых данных, преобразовывает их и
-    перекладывает в таблицу обработанных
+    Функция ищет строки в таблице raw_data.raw_data,
+    которые еще не были записаны в таблицу prepared_data.news
+    обрабатывает их и затем вставляет в таблицу prepared_data.news
     """
-    def __init__(self, corpus, target=None, ):
-        pass
+    run_date = luigi.DateParameter(default=datetime.now().date())
+    config = configparser.ConfigParser()
+    config.read('db.ini')
 
-    def tokenize(self, ):
-        pass
+    dbconfig = {'host': config['dev']['host'],
+                'dbname': config['dev']['db'],
+                'user': config['dev']['user'],
+                'password': config['dev']['password']}
 
-    def process(self, ):
-        """
-        Вызывается для каждого источника данных, проверяет в
-        таблице сырых данных новости, которые еще не обработаны,
-        делает их преобразование и записывает в таблицу преобразованных
-        данных
-        """
-        pass
+    def get_tokens(self, sentence):
+        for news in sentence:
+            if len(news) > 0:
+                yield [
+                    _.text for _ in list(tokenize(news))
+                ]
+
+    def run(self):
+        with UseDatabase(self.dbconfig) as cursor:
+            sql = "SELECT id_raw_data, id_news_source, date, url, title, text FROM raw_data.raw_data where batch_date = '1900-01-01 00:00:00' "
+
+            cursor.execute(sql)
+            query_results = cursor.fetchall()
+
+            sql = """INSERT INTO prepared_data.news
+                     (id_news, id_topic, id_news_source, id_author, date_news, link_news, title, text_news, created_date, modified_date, batch_date)
+                     VALUES 
+                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+            dt_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            for line in query_results:
+
+                sents = list(sentenize(line[5]))
+                sentence = [_.text for _ in sents]
+
+                cursor.execute(sql, (
+                    line[0],
+                    line[1],
+                    '4847c8c7-a14f-4d59-8f62-a1c622db4aab',
+                    '4847c8c7-a14f-4d59-8f62-a1c622db4aab',
+                    line[2],
+                    line[3],
+                    line[4],
+                    str(list(self.get_tokens(sentence))),
+                    dt_now,
+                    dt_now,
+                    '1900-01-01 00:00:00'))
+            self.get_target().touch()
+
+    def get_target(self):
+        return PostgresTarget(host=self.config['dev']['host'],
+                              database=self.config['dev']['db'],
+                              user=self.config['dev']['user'],
+                              password=self.config['dev']['password'],
+                              table='prepared_data.news',
+                              update_id=str(self.run_date))
+
+    def output(self):
+        return self.get_target()
+
+
+# python preprocess.py UpdateBatchDate --local-scheduler
+class UpdateBatchDate(luigi.Task):
+    """
+    Функция обновляется поле batch_date в таблице raw_data.raw_data
+    для тех строк, которые были записаны в таблицу prepared_data.news.
+    Зависит от выполнения WriteDataToDatabase
+    """
+    config = configparser.ConfigParser()
+    config.read('db.ini')
+
+    dbconfig = {'host': config['dev']['host'],
+                'dbname': config['dev']['db'],
+                'user': config['dev']['user'],
+                'password': config['dev']['password']}
+
+    def requires(self):
+        return WriteDataToDatabase()
+
+    def run(self):
+        with UseDatabase(self.dbconfig) as cursor:
+            batch_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            sql = """UPDATE raw_data.raw_data
+                     SET batch_date= %s
+                     WHERE raw_data.raw_data.id_raw_data in (
+                     select prepared_data.news.id_news
+                     from prepared_data.news
+                     where prepared_data.news.batch_date = '1900-01-01 00:00:00');"""
+
+            cursor.execute(sql, (batch_date,))
 
 
 class Dbwriter(object):
@@ -93,3 +176,7 @@ class Dbwriter(object):
         return [
             self.__process(fileid) for fileid in self.__get_fileids(fileids, categories)
         ]
+
+
+if __name__ == '__main__':
+    luigi.run()

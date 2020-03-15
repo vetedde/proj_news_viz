43b896dd081c737cb6face84c0b72309e2b6d3c9
vtrokhymenko <trokhymenkoviktor@gmail.com>
2020-03-15 17:07:10 +0200

    add plot_frequency for words like seperate file

---
+++ nlp/news_viz/visualization/plot_frequency.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+
+def plot_frequency_words(vocab: list = None, top_words: int = 30, plt_background: str = None) -> None:
+    """
+    plot token frequency
+    Parameters
+    ----------
+        vocab: list (defauld = None)
+            list tuples words with them frequency
+            like `[('на', 1330966), ... ]`
+        top_words: int (defauld = 30)
+            enter top words to plot
+        plt_background : str (default = None)
+            background stale for matplotlib.pyplot.plt
+    """
+
+    if plt_background == 'dark':
+        plt.style.use('dark_background')
+    
+    x, y = [], []
+    for key, val in vocab[:top_words]:
+        x.append(key)
+        y.append(val)
+    
+    plt.figure(figsize=(20, 10),)
+    plt.bar(x, y)
+    plt.title(f'топ-{top_words} частотных слов')
+    plt.xlabel('слова', horizontalalignment='center')
+    plt.ylabel('частотность')
+    plt.grid(linewidth=0.2)
